6f49955adbff9c620c08a685f920a623aa811ba0
Gerald Thomas <gthomas@alum.mit.edu>
2015-08-31 23:29:18 +0200

    buckets summary with links to bucket details

--- app/controllers/buckets_controller.rb
+++ app/controllers/buckets_controller.rb
@@ -1,10 +1,11 @@
 class BucketsController < ApplicationController
   def show
-  	@buckets = Ticket.select('rule, branch, count(ticket) as count').group('rule', 'branch')
+  	@buckets = Ticket.select('rule, branch, count(ticket) as count').group('rule', 'branch').order('branch asc')
+  	@buckets_total = Ticket.select('branch, count(ticket) as count').group('branch').order('branch asc')
   end
   def detail
 	@rule= Rule.find(params[:rule])
-	@branch= params[:branch]
+	@branch= params[:branch] ? "True":"False"
 	@tickets = Ticket.where("rule = ? and branch = ?", params[:rule], params[:branch])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  	resources :buckets
+  	#resources :buckets
 	resources :rules
   	#root to: 'visitors#index'
   	root to: 'rules#index'
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   	#get ':controller/:action/:id'
   	get '/rules/run/:id', to: 'rules#run', as:  :run
   	get '/rules/run/:id/count', to: 'rules#count', as: :count
-  	get 'buckets/:rule/:branch', to: 'buckets#detail', as: :detail
+  	#get 'buckets/detail/:id/:branch', to: 'buckets#detail', as: :detail
+  	get '/buckets/:rule/:branch/detail', to: 'buckets#detail', as: :detail
+  	get '/buckets/:id', to: 'buckets#show', as: :bucket
 
 end
